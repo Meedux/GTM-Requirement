785b65e5f8be18ce46868fd70542c6b93b068617
Meedux <herrerojayemark@gmail.com>
2024-04-24 03:17:52 +0800

    sidebar centering fixes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -34,14 +34,14 @@ const Sidebar = () => {
 
     <div className={`fixed top-0 left-0 ${isOpen ? "block" : "hidden" } h-screen w-screen bg-black bg-opacity-50 z-50`} onClick={handleToggle}></div>
 
-    <div className="fixed top-0 left-0 block md:hidden h-screen w-[30rem] bg-white z-50 transform transition-transform duration-300 ease-in-out" style={{ transform: isOpen ? 'translateX(0)' : 'translateX(-100%)' }}>
+    <div className="fixed top-0 left-0 block md:hidden h-screen w-[20rem] bg-white z-50 transform transition-transform duration-300 ease-in-out" style={{ transform: isOpen ? 'translateX(0)' : 'translateX(-100%)' }}>
       <button className="fixed top-4 left-4 text-3xl" onClick={handleToggle}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </button>
 
-      <div className="h-screen w-[30rem] flex flex-col justify-center items-center">
+      <div className="h-screen w-full flex flex-col justify-center items-center">
         <h1 className="text-black font-bold text-5xl text-center font-joseph-sans mb-4">Chora</h1>
 
         <Button name='HOME' url="/home" />
--- components/Sidebar/Button.tsx
+++ components/Sidebar/Button.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Button = ({ name, url }: { name: string; url: string }) => {
   return (
     <Link href={url}>
-      <button className="p-3 rounded-md bg-[#006FEE] text-xl font-joseph-sans mb-6 w-[15rem]">
+      <button className="p-3 rounded-md bg-[#006FEE] text-white text-xl font-joseph-sans mb-6 w-[15rem]">
         {name}
       </button>
     </Link>
