533c9b395ff97f14545298cd46c0633bbae05747
Meedux <herrerojayemark@gmail.com>
2024-04-26 19:26:44 +0800

    Add table structure to AccountCardOne, AccountCardTwo, and AccountCardThree components

--- components/Analysis/AccountCardThree.tsx
+++ components/Analysis/AccountCardThree.tsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const AccountCardThree = () => {
   return (
-    <div className='text-black'>AccountCardThree</div>
+    <div className="">
+      <div className="border p-2 border-gray-300 rounded-md shadow-lg w-full card h-[96%] md:h-[93%] overflow-auto">
+        <table className="w-full">
+          <thead>
+            <tr className="border-b border-gray-300">
+              <th className=" text-xs text-black font-bold font-joseph-sans">
+                Account #
+              </th>
+              <th className=" text-xs text-black font-bold font-joseph-sans">
+                City, State
+              </th>
+              <th className=" text-xs text-black font-bold font-joseph-sans">
+                Utility
+              </th>
+              <th className=" text-xs text-black font-bold font-joseph-sans">
+                Last Sync
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
