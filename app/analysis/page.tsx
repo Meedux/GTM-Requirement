b6928b3a370de31c100d2b1ba0fd538f08eb8faa
Meedux <herrerojayemark@gmail.com>
2024-04-27 07:00:52 +0800

    Refactor Analysis Page component and add LineChart component

--- app/analysis/page.tsx
+++ app/analysis/page.tsx
@@ -1,54 +1,20 @@
 "use client";
-import { useRef, useEffect } from 'react';
+
 import React from 'react'
-import { Chart, LineController, LinearScale, PointElement, LineElement, CategoryScale } from 'chart.js';
 import AccountCardOne from '@/components/Analysis/AccountCardOne';
 import AccountCardTwo from '@/components/Analysis/AccountCardTwo';
 import AccountCardThree from '@/components/Analysis/AccountCardThree';
+import LineChart from '@/components/Charts/LineChart';
 
-Chart.register(LineController, LinearScale, PointElement, LineElement, CategoryScale);
+
 
 const page = () => {
-  const chartRef = useRef<Chart | null>(null);
-
-  useEffect(() => {
-    const ctx = document.getElementById('chart') as HTMLCanvasElement;
-    if (ctx) {
-      chartRef.current = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['1', '2', '3', '4', '5', '6', '7'],
-          datasets: [
-            {
-              label: 'Dataset 1',
-              data: [12, 19, 3, 5, 2, 3, 9],
-              fill: false,
-              borderColor: 'rgb(75, 192, 192)',
-              tension: 0.1
-            }
-          ]
-        },
-        options: {
-          responsive: true,
-          resizeDelay: 200,
-        }
-      });
-    }
-
-    return () => {
-      if (chartRef.current) {
-        (chartRef.current as Chart).destroy();
-        chartRef.current = null;
-      }
-    };
-  }, []);
+  
   
   return (
     <div className="w-full h-screen overflow-scroll px-[5%]">
       <div className="h-full py-[5%] grid grid-row-2 gap-4">
-          <div className="relative md:block flex justify-center items-center md:h-[100%] md:w-[100%] h-[50vh] w-[80vw]">
-            <canvas id='chart'></canvas>
-          </div>
+          <LineChart />
           <div className="h-full grid grid-cols-1 md:grid-cols-3 gap-4 mb-7">
             <AccountCardOne />
             <AccountCardTwo />
---
+++ components/Charts/LineChart.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from 'react'
+import { Chart, LineController, LinearScale, PointElement, LineElement, CategoryScale } from 'chart.js';
+import { useRef, useEffect } from 'react';
+
+Chart.register(LineController, LinearScale, PointElement, LineElement, CategoryScale);
+
+const LineChart = () => {
+    const chartRef = useRef<Chart | null>(null);
+
+  useEffect(() => {
+    const ctx = document.getElementById('chart') as HTMLCanvasElement;
+    if (ctx) {
+      chartRef.current = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['1', '2', '3', '4', '5', '6', '7'],
+          datasets: [
+            {
+              label: 'Dataset 1',
+              data: [12, 19, 3, 5, 2, 3, 9],
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          resizeDelay: 200,
+        }
+      });
+    }
+
+    return () => {
+      if (chartRef.current) {
+        (chartRef.current as Chart).destroy();
+        chartRef.current = null;
+      }
+    };
+  }, []);
+  return (
+    <div className="relative md:block flex justify-center items-center md:h-[100%] md:w-[100%] h-[50vh] w-[80vw]">
+        <canvas id='chart'></canvas>
+    </div>
+  )
+}
+
+export default LineChart
